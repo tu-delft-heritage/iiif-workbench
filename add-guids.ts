b62cb117bb9e554e9cc46d2b8e68c9afee15c2d8
Jules Schoonman <jules.schoonman@gmail.com>
2025-06-23 16:42:39 +0200

    script for adding guids

---
+++ add-guids.ts
@@ -0,0 +1,21 @@
+import { selectFile, loadYml, saveYml } from "./src/shared";
+import { v4 } from "uuid";
+
+const path = await selectFile("input/*.yml");
+const yml = await loadYml(path);
+
+// Adding missing guid to "start" of objects
+if (!yml.collection.guid) {
+  yml.collection = { guid: v4(), ...yml.collection };
+}
+
+yml.items.forEach((object, index) => {
+  if (!object.guid) {
+    yml.items[index] = { guid: v4(), ...object };
+  }
+});
+
+// Not overwriting existing file in order to preserve comments, etc
+const outputPath = path.replace(".yml", "-guids.yml");
+
+saveYml(outputPath, yml);

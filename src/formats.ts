12ccd58264302b5973fa61cbebc8acf5b9117214
Jules Schoonman <j.a.schoonman@tudelft.nl>
2026-08-12 09:29:58 +0200

    improved format labels

--- src/formats.ts
+++ src/formats.ts
@@ -1,17 +1,33 @@
 // From: https://help-nl.oclc.org/Discovery_and_Reference/WorldCat_Discovery/Search_results/Format_display_in_search_results?sl=nl
 export const formats = {
-  Archv: "Archival Material",
+  Archv: {
+    en: "Archival Material",
+    nl: "Archiefmateriaal"
+  },
   ArtChapter: "Article",
   AudioBook: "Audiobook",
-  Book: "Book",
+  Book: {
+    en: "Book",
+    nl: "Boek"
+  },
   CompFile: "Computer File",
   Encyc: "Encyclopedia",
   Game: "Game",
-  Image: "Image",
+  Image: {
+    en: "Visual Material",
+    nl: "Beeldmateriaal"
+  },
   IntMM: "Interactive Multimedia",
-  Jrnl: "Journal/Magazine",
+  // "Journal/Magazine",
+  Jrnl: {
+    en: "Journal",
+    nl: "Tijdschrift"
+  },
   Kit: "Kit",
-  Map: "Map",
+  Map: {
+    en: "Map",
+    nl: "Kaart"
+  },
   MsScr: "Musical Score",
   Music: "Music Recording",
   News: "Newspaper",
